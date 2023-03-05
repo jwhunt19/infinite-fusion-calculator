2529294a38b7c06d944782d2857f281abd9c223c
Joey Huntington <jwhunt19@gmail.com>
2023-03-04 18:34:20 -0700

    add fusion dex, id and name info

--- src/components/fusion/Fusion.jsx
+++ src/components/fusion/Fusion.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Fusion = (head, body) => {
+// dex = head[1] + (420 * body[1])
 
-  let dex
+const Fusion = ({ head, body }) => {
 
+  const [dex, setDex] = useState(0)
+  const [fusionId, setFusionId] = useState('')
+  const [names, setNames] = useState('')
+  
+  useEffect(() => {
+    if (head[1] && body[1]) {
+      setDex(head[1] + (420 * body[1]));
+      setFusionId(`(${head[1]}.${body[1]})`);
+      setNames(`${head[0].charAt(0).toUpperCase()+head[0].slice(1)}/` +
+      `${body[0].charAt(0).toUpperCase()+body[0].slice(1)}`)
+    }
+  }, [head, body])
   
 
+
+
   return (
-    <>
-    {/* todo - dex # */}
-    {/* todo - fusion number (###.###) */}
+    <div id={`${head[1]}.${body[1]}`}>
     {/* todo - fusion name */}
+    <span>{dex ? dex : ''}</span>
+    <br /> {/* todo delete after styling */}
+    <span>{fusionId}</span>
+    <br /> {/* todo delete after styling */}
+    <span>{names}</span>
     {/* todo - sprite compoment */}
-    {/* todo - names head/body */}
     {/* todo - types (check for exceptions) */}
     {/* todo - base stats component  */}
     {/* todo - abilities (check for exceptions) */}
     {/* todo - type effectiveness component */}
-    </>
+    </div>
   )
 }
 
