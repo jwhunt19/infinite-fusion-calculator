46e365ed8227c5876751e805685a25bdb4dce7f4
Joey Huntington <jwhunt19@gmail.com>
2023-03-16 23:07:04 -0600

    add favicon, font, and input styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
 import React, { useEffect, useState } from "react";
+import {
+  ChakraProvider,
+  Grid,
+  GridItem,
+  Box,
+  extendTheme,
+} from "@chakra-ui/react";
 
 import Fusion from "./fusion/Fusion";
 import FusionInput from "./FusionInput";
@@ -48,30 +55,67 @@ const App = () => {
   }, [pokemonOne.name, pokemonTwo.name]);
 
   return (
-    <>
-    <header>
-      <h1>Infinite Fusion Calculator</h1>
-      <span>
-        Based on work by and sourcing images/data from
-        <a href="https://github.com/Aegide" target="_blank">
-          {" "}
-          Aegide
-        </a>
-      </span>
-    </header>
-      <FusionInput
-        setPokemon={setPokemonOne}
-        pokemon={pokemonOne}
-        inputFor={"pokemonOne"}
-      />
-      <FusionInput
-        setPokemon={setPokemonTwo}
-        pokemon={pokemonTwo}
-        inputFor={"pokemonTwo"}
-      />
-      <Fusion head={pokemonOne} body={pokemonTwo} />
-      <Fusion head={pokemonTwo} body={pokemonOne} />
-    </>
+    <ChakraProvider>
+      <Box backgroundColor="rgb(27, 24, 34)">
+        <Grid maxW="80vw" m="auto" pt="5vh">
+          <GridItem m="auto" textAlign="center" mb="5vh">
+            <header>
+              <h1>Infinite Fusion Calculator</h1>
+              <span>
+                Based on work by and sourcing images/data from{" "}
+                <a href="https://github.com/Aegide" target="_blank">
+                  Aegide
+                </a>
+              </span>
+            </header>
+          </GridItem>
+
+          <GridItem
+            h="7vh"
+            className="fusion-input-container"
+            backgroundColor="#95acb6"
+            mb="1vh"
+          >
+            <Box w="30%">
+              <FusionInput
+                setPokemon={setPokemonOne}
+                pokemon={pokemonOne}
+                inputFor={"pokemonOne"}
+              />
+            </Box>
+            <Box w="70%" className="fusion-input-base">
+              <span>{JSON.stringify(pokemonOne.stats)}</span>
+            </Box>
+          </GridItem>
+
+          <GridItem
+            h="7vh"
+            className="fusion-input-container"
+            backgroundColor="#95acb6"
+            mb="1vh"
+          >
+            <Box w="30%">
+              <FusionInput
+                setPokemon={setPokemonTwo}
+                pokemon={pokemonTwo}
+                inputFor={"pokemonTwo"}
+              />
+            </Box>
+            <Box w="70%" className="fusion-input-base">
+              <span>{JSON.stringify(pokemonTwo.stats)}</span>
+            </Box>
+          </GridItem>
+
+          <GridItem>
+            <Fusion head={pokemonOne} body={pokemonTwo} />
+          </GridItem>
+
+          <GridItem>
+            <Fusion head={pokemonTwo} body={pokemonOne} />
+          </GridItem>
+        </Grid>
+      </Box>
+    </ChakraProvider>
   );
 };
 
